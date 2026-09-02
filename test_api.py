91148c3d95fd815af1391f37dc91113c02f60e17
xhajek-oss <x.hajek@gmail.com>
2026-09-02 23:05:15 +0200

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -19,3 +19,26 @@ for event in events:
     print(f"Event ID: {event.event_id}")
     print(f"URL: {event.url}")
     print("-" * 50)
+print("\n--- CAVEMAN DETAIL ---")
+
+for schedule in data["schedules"]:
+    if schedule["id"] == 9567679:
+        print("\nSCHEDULE:")
+        print(schedule)
+
+for event in data["included"]["events"]:
+    if event["id"] == 3419763:
+        print("\nEVENT:")
+        print(event)
+
+print("\nVENUES:")
+for venue in data["included"]["venues"]:
+    print(venue)
+
+print("\nSALES:")
+for sale in data["included"]["sales"]:
+    print(sale)
+
+print("\nIMAGES:")
+for image in data["included"]["images"]:
+    print(image)
